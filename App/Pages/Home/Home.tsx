527fe7eae009bd8e78d4ceff68920adc2f5ee133
石破天惊 <shanshang130@gamil.com>
2020-04-08 15:50:48 +0800

    support typescript

---
+++ App/Pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+
+
+import React from "react";
+import {View, Text} from "react-native";
+
+
+export class Home extends React.Component{
+    render(){
+        return <View>
+            <Text>123123</Text>
+        </View>
+    }
+}
